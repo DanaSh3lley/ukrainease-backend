e026e2d01a3ed7b48fc326941982bfd6fdd87f84
Havaleshko Bohdana <havaleshkobohdana@gmail.com>
2023-05-24 20:30:55 +0300

    add leagues moving and assigning users

--- controllers/answerController.js
+++
@@ -1,8 +0,0 @@
-const factory = require('./handlerFactory');
-const Answer = require('../models/answerModel');
-
-exports.getAnswer = factory.getOne(Answer);
-exports.getAllAnswers = factory.getAll(Answer);
-exports.createAnswer = factory.createOne(Answer);
-exports.updateAnswer = factory.updateOne(Answer);
-exports.deleteAnswer = factory.deleteOne(Answer);
--- controllers/userLeagueController.js
+++
@@ -1,8 +0,0 @@
-const UserLeague = require('../models/userLeagueModel');
-const factory = require('./handlerFactory');
-
-exports.getUserLeague = factory.getOne(UserLeague);
-exports.getAllUserLeagues = factory.getAll(UserLeague);
-exports.createUserLeague = factory.createOne(UserLeague);
-exports.updateUserLeague = factory.updateOne(UserLeague);
-exports.deleteUserLeague = factory.deleteOne(UserLeague);
